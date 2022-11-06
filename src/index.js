a55020b4be090f4a5da01479c0ea21445898fa36
elementarteilx <85301104+elementarteilx@users.noreply.github.com>
2022-11-06 18:36:41 +0100

    input for city javascript

--- src/index.js
+++ src/index.js
@@ -1,10 +1,22 @@
 let currentTime = new Date();
 
-function updateCity(city) {}
+function updateCity(event) {
+  event.preventDefault();
+  console.log("1");
+  let cityInput = document.querySelector("#city_enter");
+  console.log("cityInput.value");
+  let h2 = document.querySelector("h2");
+  console.log("2");
+  h2.innerHTML = `${cityInput.value}`;
+}
 
 function formatDate(date) {
   let minutes = date.getMinutes();
   let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0 + ${hours}`;
+  }
+
   let index = date.getDay();
   let days = [
     "Sunday",
@@ -30,5 +42,3 @@ clock.innerHTML = formatDate(currentTime);
 //print current city
 let city_input = document.querySelector("#city_input");
 city_input.addEventListener("click", updateCity);
-let h2 = document.querySelector("h2");
-h2.innerHTML = "hello";
