ccb7dba5ac8ddf8adaf050833e801d9ed9b6b3db
elementarteilx <alena.moeckli@ost.ch>
2023-01-04 09:50:13 +0100

    default city

--- src/index.js
+++ src/index.js
@@ -4,20 +4,31 @@ let globaltemperature = 0;
 //
 function updateCityname(event) {
   event.preventDefault();
-  console.log("1");
   let cityInput = document.querySelector("#city_enter");
   console.log("cityInput.value");
-  let h2 = document.querySelector("h2");
-  console.log("2");
-  h2.innerHTML = `${cityInput.value}`;
+
   //var result = text.toUpperCase();
+
+  callcity(cityInput.value);
+}
+
+
+function callcity(event) {
+  console.log("11");
+  console.log(event);
+  console.log("1");
 
   //preparation for temperature update
   let apiKey = "8a6ee44d7a95db9439f2411cfbeee474";
   let units = "metric";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityInput.value}&appid=${apiKey}&units=${units}`;
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${event}&appid=${apiKey}&units=${units}`;
   axios.get(apiUrl).then(showTemperature);
+
+  let h2 = document.querySelector("h2");
+  h2.innerHTML = `${event}`;
 }
+
+
 
 //changes showed weather data to current city
 function showTemperature(event) {
@@ -74,7 +85,7 @@ function toCelsius(event) {
 function toFahrenheit(event) {
   console.log("fah");
   let tempOutput = document.querySelector("#temperature");
-  tempOutput.innerHTML = (Math.round(globaltemperature*1.8+32*100)/100);
+  tempOutput.innerHTML = (Math.round(globaltemperature*1.8+32));
  // tempOutput = globaltemperature*1.8+32;
 }
 
@@ -94,4 +105,4 @@ let fahrenheit = document.querySelector("#Fahrenheit");
 fahrenheit.addEventListener("click", toFahrenheit);
 
 //default City
-updateCityname("berlin");
+callcity("berlin");
